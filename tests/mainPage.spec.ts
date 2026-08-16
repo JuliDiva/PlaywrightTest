03e388353bd80db8b7e32f64557de8b374a73cd8
Juli Diva <diva.julii@gmail.com>
2026-08-16 16:25:40 -0400

    Initial Playwright project

---
+++ tests/mainPage.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page.getByRole('link', { name: 'GitHub repository' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Docs' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Node.js' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'CLI', exact: true })).toBeVisible();
+});
+
+test('test checks names of the header navigation', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toContainText(
+    'Playwright',
+  );
+  await expect(page.getByRole('link', { name: 'Docs' })).toContainText('Docs');
+  await expect(page.getByRole('link', { name: 'API' })).toContainText('API');
+  await expect(page.getByLabel('Main', { exact: true })).toContainText('MCP');
+});
+
+test('test checks attribute href elements', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toContainText(
+    'Playwright',
+  );
+  await expect(page.getByRole('link', { name: 'Docs' })).toHaveAttribute('href', '/docs/intro');
+  await expect(page.getByRole('link', { name: 'API' })).toHaveAttribute(
+    'href',
+    '/docs/api/class-playwright',
+  );
+});
+
+test('test select dark mode', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByLabel('Switch between dark and light').click();
+  await expect.soft(page.locator('html')).toHaveAttribute('data-theme', 'light');
+});
